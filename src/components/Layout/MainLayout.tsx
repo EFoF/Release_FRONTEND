37c7203ca0165ed46b531c86669b0435ebbe9bde
JoSooha <dlrhdcjs@naver.com>
2023-07-25 17:16:16 +0900

    feat: MainLayout 정의

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,3 +1,30 @@
-export default function MainLayout() {
-    
+import React, { ReactNode } from "react";
+import styled from "styled-components";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export const Full = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Inner = styled.div`
+  width: 100%;
+  display: flex;
+  position: relative;
+  padding-top: 5.56rem; // header, footer 높이만큼 padding 설정, 이후 사이드바 고려
+`;
+
+interface MainLayoutProps {
+  children: ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  <Full>
+    <Header />
+    <Inner>{children}</Inner>
+    <Footer />
+  </Full>;
 }
