4c220c6e1337d3c7f1a21caa8452ff09275b001a
JoSooha <dlrhdcjs@naver.com>
2023-07-30 22:19:00 +0900

    feat: myside바 설정 및 생성 버튼

--- src/components/Sidebar/MySide.tsx
+++ src/components/Sidebar/MySide.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import "../../styles/font.css";
+import setting from "../../img/setting1.png"
+import Button from "../Button";
 
 const Container = styled.div`
   display: flex;
@@ -22,10 +24,16 @@ const SidebarContainer = styled.div`
   align-items: center;
 `;
 
+const MyRow = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
 const MyTitle = styled.div`
   display: flex;
   flex-direction: row;
   margin-bottom: 2rem;
+  margin-right: 1.4rem;
   color: #000;
     font-family: Inter;
     font-size: 2rem;
@@ -33,6 +41,13 @@ const MyTitle = styled.div`
     font-weight: 600;
     line-height: normal;
 `;
+
+const MySetting = styled.img`
+    width: 1.8rem;
+    height: 1.8rem;
+    margin-top: 0.5rem;
+    cursor: pointer;
+`
 
 const MyInfo = styled.div`
 color: #000;
@@ -61,19 +76,34 @@ font-weight: 400;
 line-height: normal;
 `;
 
-
+const ButtonContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   position: relative;
+//   bottom: 0;
+  position: absolute;
+  bottom: 1rem; /* 버튼과 하단 간격 조절 */
+  left: 50%; /* 가운데 정렬을 위해 왼쪽 위치 조절 */
+  transform: translateX(-50%); /* 가운데 정렬을 위해 가로 방향으로 이동 */
+`
 
 export default function MySide() {
   
   return (
     <Container>
       <SidebarContainer>
-        <MyTitle>내 정보</MyTitle>
+        <MyRow>
+            <MyTitle>내 정보</MyTitle>
+            <MySetting src={setting}/>
+        </MyRow>
         <MyInfo>
             <MyName>최철웅</MyName>
             <MyEmail>(dlrhdcjs@naver.com)</MyEmail>
         </MyInfo>
       </SidebarContainer>
+      <ButtonContainer>
+        <Button title="회사 생성하기" theme="blue"/>
+      </ButtonContainer>
     </Container>
   );
 }
