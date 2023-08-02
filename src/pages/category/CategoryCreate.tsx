c622a54cd4772c90f37f2dbd24464bd4c2547c3f
Owen-Choi <oldstyle482@gmail.com>
2023-08-02 14:43:40 +0900

    [ADD] 디테일 수정 - 수정 버튼 클릭 시 체크버튼, 취소버튼으로 변경

--- src/pages/category/CategoryCreate.tsx
+++ src/pages/category/CategoryCreate.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import Button from "../../components/Button";
 import pencil from "../../img/pencil.png";
 import eye from "../../img/eye.png";
+import check from "../../img/check.png";
+import clear from "../../img/clear.png";
 import {Title1, Title2} from "../../components/Text/Title";
 import {Container1} from "../../components/Container";
 import markdown from "../company/markdown";
@@ -14,38 +16,31 @@ interface EditButtonProps {
     width: number;
     height: number;
 }
+
 export default function CategoryCreate() {
   const [isPreview, setIsPreview] = useState(false);
   const [markdown, setMarkdown] = useState("# Title");
-  const [isEditMode, setIsEditMode] = useState([]);
-  const [categories, setCategories] = useState([
-        {name: "개발 프로세스", intro: "카카오 i 서비스 시스템에서 카카오 i 계정(Kakao i Account)은 카카오 i 계정을 기반으로 제공되는 다양한 카카오 i 서비스들(카카오워크, 카카오 i 클라우드 등)과 연동하여 사용자 인증/권한 관리 등과 같은 통합 계정 관리와 계정의 생성, 변경, 삭제와 같은 계정의 라이프 사이클을 관리하고 리소스 접근에 대한 권한을 제어합니다."},
-        {name: "API", intro: "카카오 i 플랫폼과 연동하여 다양한 IoT 디바이스에서 AI 음성 서비스를 활용할 수 있도록 개발에 필요한 기본적인 개념 및 상세 설명을 제공합니다. 카카오 i 플랫 서비스를 활용할 수 있도록 개발에 필요한 기본적인 개념 및 상세 설명을 제공합니다. "},
-        {name: "부록", intro: "카카오 i 계정 서비스를 좀 더 편리하게 사용할 수 있도록 관리자 서비스 페이지를 제공합니다.        "},
-      ]);
+  const [isTitleEdit, setIsTitleEdit] = useState<Boolean>(false);
 
   const handleClickPreview = () => {
       setIsPreview(!isPreview);
   }
 
-  const handleChangeText = (e : React.ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      setMarkdown(e.target.value)
-  }
-
-  useEffect(() => {
-
-  }, [categories])
-
+    const handleTitleEditClick = () => {
+        setIsTitleEdit(!isTitleEdit);
+    }
 
   return (
     <Container>
           <CompanyContainer>
               <EditContainer>
                   <Title1>개발 프로세스</Title1>
-                  <EditButtonContainer>
-                      <EditButton imageUrl={pencil} width={24} height={24}></EditButton>
-                  </EditButtonContainer>
+                  {isTitleEdit ? (
+                      <EditButtonContainer>
+                          <EditButton imageUrl={check} width={24} height={24} onClick={handleTitleEditClick} />
+                          <EditButton imageUrl={clear} width={24} height={24} onClick={handleTitleEditClick} />
+                      </EditButtonContainer>
+                  ) : <EditButton imageUrl={pencil} width={24} height={24} onClick={handleTitleEditClick} /> }
               </EditContainer>
             <CompanyIntro>
                 카카오 i 서비스 시스템에서 카카오 i 계정(Kakao i Account)은 카카오 i 계정을 기반으로 제공되는 다양한 카카오 i 서비스들(카카오워크, 카카오 i 클라우드 등)과 연동하여 사용자 인증/권한 관리 등과 같은 통합 계정 관리와 계정의 생성, 변경, 삭제와 같은 계정의 라이프 사이클을 관리하고 리소스 접근에 대한 권한을 제어합니다.
@@ -60,7 +55,6 @@ export default function CategoryCreate() {
                     <EditButton imageUrl={pencil} width={24} height={24} onClick={handleClickPreview}/> :
                     <EditButton imageUrl={eye} width={24} height={24} onClick={handleClickPreview}/>}
             </PreviewContainer>
-            {/*{markdown()}*/}
             {isPreview ?
                 <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown> : <TextInput defaultValue={markdown} />
             }
--- src/pages/project/ProjectEdit.tsx
+++ src/pages/project/ProjectEdit.tsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import Button from "../../components/Button";
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import pencil from "../../img/pencil.png";
 import minus from "../../img/minus.png";
 import plus from "../../img/plus.png";
+import check from "../../img/check.png";
+import clear from "../../img/clear.png";
 import {Title1, Title2} from "../../components/Text/Title";
 import {Container1} from "../../components/Container";
 import { Toggle } from "typescript-toggle";
@@ -13,14 +15,37 @@ interface EditButtonProps {
     width: number;
     height: number;
 }
+
+interface Category {
+    name: string;
+    intro: string;
+}
+
 export default function ProjectEdit() {
   const [isOn, setIsOn] = useState(false);
-  const categories = [
+  const [isEditMode, setIsEditMode] = useState<Boolean[]>([]);
+  const [isTitleEdit, setIsTitleEdit] = useState<Boolean>(false);
+  const [categories, setCategories] = useState<Category[]>([
         {name: "개발 프로세스", intro: "카카오 i 서비스 시스템에서 카카오 i 계정(Kakao i Account)은 카카오 i 계정을 기반으로 제공되는 다양한 카카오 i 서비스들(카카오워크, 카카오 i 클라우드 등)과 연동하여 사용자 인증/권한 관리 등과 같은 통합 계정 관리와 계정의 생성, 변경, 삭제와 같은 계정의 라이프 사이클을 관리하고 리소스 접근에 대한 권한을 제어합니다."},
         {name: "API", intro: "카카오 i 플랫폼과 연동하여 다양한 IoT 디바이스에서 AI 음성 서비스를 활용할 수 있도록 개발에 필요한 기본적인 개념 및 상세 설명을 제공합니다. 카카오 i 플랫 서비스를 활용할 수 있도록 개발에 필요한 기본적인 개념 및 상세 설명을 제공합니다. "},
         {name: "부록", intro: "카카오 i 계정 서비스를 좀 더 편리하게 사용할 수 있도록 관리자 서비스 페이지를 제공합니다.        "},
-  ]
+    ]);
   const [inputValue, SetInputValue] = useState("");
+
+  const handleCategoryEditClick = (index:number) => {
+      const updatedList = [...isEditMode];
+      updatedList[index] = !updatedList[index];
+      setIsEditMode(updatedList);
+  }
+
+  const handleTitleEditClick = () => {
+      setIsTitleEdit(!isTitleEdit);
+  }
+
+  useEffect(() => {
+        const newBooleanList = new Array(categories.length).fill(false);
+        setIsEditMode(newBooleanList);
+  }, [categories])
 
   return (
     <Container>
@@ -28,9 +53,13 @@ export default function ProjectEdit() {
           <CompanyContainer>
               <EditContainer>
                   <CompanyName>Kakao i Account</CompanyName>
-                  <EditButtonContainer>
-                      <EditButton imageUrl={pencil} width={24} height={24}></EditButton>
-                  </EditButtonContainer>
+                  {isTitleEdit ?
+                      (<EditButtonContainer>
+                          <EditButton imageUrl={check} width={24} height={24} onClick={handleTitleEditClick} />
+                          <EditButton imageUrl={clear} width={24} height={24} onClick={handleTitleEditClick} />
+                      </EditButtonContainer>) :
+                      <EditButton imageUrl={pencil} width={24} height={24} onClick={handleTitleEditClick}></EditButton>
+                  }
               </EditContainer>
             <CompanyIntro>
                 카카오 i 서비스 시스템(Kakao i Service System)은 카카오 i 서비스의 백엔드 체계를 나타내며, 카카오 i 계정, 카카오워크, 카카오 i 클라우드 등의 여러 ‘카카오 i 서비스’들과 이들의 계정을 상호 연동해주는 ‘Adapter 서버’로 구성됩니다.
@@ -55,10 +84,17 @@ export default function ProjectEdit() {
                 <CategoryContainer>
                     <EditContainer>
                             <CategoryName>{category.name}</CategoryName>
-                        <EditButtonContainer>
-                            <EditButton imageUrl={pencil} width={24} height={24}></EditButton>
-                            <EditButton imageUrl={minus} width={24} height={24}></EditButton>
-                        </EditButtonContainer>
+                        {!isEditMode[index] ? (
+                            <EditButtonContainer>
+                                <EditButton imageUrl={pencil} width={24} height={24} onClick={()=>handleCategoryEditClick(index)} />
+                                <EditButton imageUrl={minus} width={24} height={24} onClick={()=>handleCategoryEditClick(index)}/>
+                            </EditButtonContainer>
+                        ) : (
+                            <EditButtonContainer>
+                                <EditButton imageUrl={check} width={24} height={24} onClick={()=>handleCategoryEditClick(index)} />
+                                <EditButton imageUrl={clear} width={24} height={24} onClick={()=>handleCategoryEditClick(index)} />
+                            </EditButtonContainer>
+                        )}
                     </EditContainer>
                     <CategoryIntro>
                         {category.intro}
@@ -194,7 +230,7 @@ export const ToggleContainer = styled.div`
   margin-left: 10rem;
 `;
 
-const ScopeText = styled.h3`
+export const ScopeText = styled.h3`
   font-family: S-medium;
   font-size: 1.25rem;
   font-style: normal;
