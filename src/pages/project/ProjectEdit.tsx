deb279d202da86420b18c584237cdcbe40979a45
Owen-Choi <oldstyle482@gmail.com>
2023-08-03 12:47:03 +0900

    [ADD] 디테일 수정 - 모달 클릭 시 데이터 삭제, 실제 JSON 형식으로 데이터 구성

--- src/pages/project/ProjectEdit.tsx
+++ src/pages/project/ProjectEdit.tsx
@@ -18,21 +18,56 @@ interface EditButtonProps {
 }
 
 interface Category {
-    name: string;
-    intro: string;
+    id : number;
+    title: string;
+    description: string;
+}
+
+interface CategoryData {
+    categoryEachDtoList: Category[];
+}
+
+interface Project {
+    title: string;
+    description: string;
+    scope: boolean;
 }
 
 export default function ProjectEdit() {
-  const [isOn, setIsOn] = useState(false);
   const [isEditMode, setIsEditMode] = useState<Boolean[]>([]);
   const [isTitleEdit, setIsTitleEdit] = useState<Boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [index, setIndex] = useState<Number>();
-  const [categories, setCategories] = useState<Category[]>([
-        {name: "개발 프로세스", intro: "카카오 i 서비스 시스템에서 카카오 i 계정(Kakao i Account)은 카카오 i 계정을 기반으로 제공되는 다양한 카카오 i 서비스들(카카오워크, 카카오 i 클라우드 등)과 연동하여 사용자 인증/권한 관리 등과 같은 통합 계정 관리와 계정의 생성, 변경, 삭제와 같은 계정의 라이프 사이클을 관리하고 리소스 접근에 대한 권한을 제어합니다."},
-        {name: "API", intro: "카카오 i 플랫폼과 연동하여 다양한 IoT 디바이스에서 AI 음성 서비스를 활용할 수 있도록 개발에 필요한 기본적인 개념 및 상세 설명을 제공합니다. 카카오 i 플랫 서비스를 활용할 수 있도록 개발에 필요한 기본적인 개념 및 상세 설명을 제공합니다. "},
-        {name: "부록", intro: "카카오 i 계정 서비스를 좀 더 편리하게 사용할 수 있도록 관리자 서비스 페이지를 제공합니다.        "},
-    ]);
+  const [project, setProject] = useState<Project>(
+      {
+          "description": "카카오 i 서비스 시스템에서 카카오 i 계정(Kakao i Account)은 카카오 i 계정을 기반으로 제공되는 다양한 카카오 i 서비스들(카카오워크, 카카오 i 클라우드 등)과 연동하여 사용자 인증/권한 관리 등과 같은 통합 계정 관리와 계정의 생성, 변경, 삭제와 같은 계정의 라이프 사이클을 관리하고 리소스 접근에 대한 권한을 제어합니다.",
+          "scope": true,
+          "title": "Owen-Choi Project"
+      }
+  )
+  const [categories, setCategories] = useState<CategoryData>(
+      {
+          categoryEachDtoList: [
+              {
+                  id: 1,
+                  title: "test category",
+                  description: "test category description"
+              },
+              {
+                  id: 2,
+                  title: "test category",
+                  description: "test category description"
+              },
+              {
+                  id: 3,
+                  title: "test category",
+                  description: "test category description"
+              }
+          ]
+      }
+  )
+
+
   const [inputValue, SetInputValue] = useState("");
 
   const handleCategoryEditClick = (index:number) => {
@@ -47,6 +82,7 @@ export default function ProjectEdit() {
 
   const handleRemoveButton = (index:number) => {
       setIsModalOpen(true);
+      // 몇번째 인덱스의 리스트를 지울지 따로 저장해줘야 함
       setIndex(index);
   }
 
@@ -57,13 +93,19 @@ export default function ProjectEdit() {
 
     const handleModalConfirm = () => {
         // TODO: 인덱스에 해당하는 카테고리 지우고 디비에 반영
-
+        // JSON 데이터 수정
+        setCategories((categories) => ({
+            ...categories,
+            categoryEachDtoList : categories.categoryEachDtoList.filter(
+                (category, categoryIndex) => categoryIndex !== index
+            )
+        }))
         // 모달 닫기
         setIsModalOpen(false);
     };
 
   useEffect(() => {
-        const newBooleanList = new Array(categories.length).fill(false);
+        const newBooleanList = new Array(categories.categoryEachDtoList.length).fill(false);
         setIsEditMode(newBooleanList);
   }, [categories])
 
@@ -72,7 +114,7 @@ export default function ProjectEdit() {
         <Scope>
           <CompanyContainer>
               <EditContainer>
-                  <CompanyName>Kakao i Account</CompanyName>
+                  <CompanyName>{project.title}</CompanyName>
                   {isTitleEdit ?
                       (<EditButtonContainer>
                           <EditButton imageUrl={check} width={24} height={24} onClick={handleTitleEditClick} />
@@ -82,7 +124,7 @@ export default function ProjectEdit() {
                   }
               </EditContainer>
             <CompanyIntro>
-                카카오 i 서비스 시스템(Kakao i Service System)은 카카오 i 서비스의 백엔드 체계를 나타내며, 카카오 i 계정, 카카오워크, 카카오 i 클라우드 등의 여러 ‘카카오 i 서비스’들과 이들의 계정을 상호 연동해주는 ‘Adapter 서버’로 구성됩니다.
+                {project.description}
             </CompanyIntro>
           </CompanyContainer>
             <ToggleContainer>
@@ -90,7 +132,10 @@ export default function ProjectEdit() {
                     <ScopeText>
                         공개 여부
                     </ScopeText>
-                    <Toggle isOn={isOn} handleChange={() => setIsOn(!isOn)}/>
+                    <Toggle isOn={project.scope} handleChange={() => setProject((project) => ({
+                        ...project,
+                        scope: !project.scope
+                    }))}/>
                 </EditContainer>
             </ToggleContainer>
         </Scope>
@@ -100,10 +145,10 @@ export default function ProjectEdit() {
         </ButtonContainer>
         <DetailContainer>
           <CategoryContainers>
-            {categories.map((category, index)=>(
+            {categories.categoryEachDtoList.map((category, index)=>(
                 <CategoryContainer>
                     <EditContainer>
-                            <CategoryName>{category.name}</CategoryName>
+                            <CategoryName>{category.title}</CategoryName>
                         {!isEditMode[index] ? (
                             <EditButtonContainer>
                                 <EditButton imageUrl={pencil} width={24} height={24} onClick={()=>handleCategoryEditClick(index)} />
@@ -117,7 +162,7 @@ export default function ProjectEdit() {
                         )}
                     </EditContainer>
                     <CategoryIntro>
-                        {category.intro}
+                        {category.description}
                     </CategoryIntro>
                 </CategoryContainer>
             ))}
