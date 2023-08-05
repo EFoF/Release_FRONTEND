6661277eef8dab13f72138694fff8e096493ace5
JoSooha <dlrhdcjs@naver.com>
2023-08-06 04:11:02 +0900

    comment: axios 인터셉터 주석

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,22 +5,9 @@ import PATH from "../constants/path";
 axios.defaults.baseURL = API.BASE_URL; //
 axios.defaults.withCredentials = true;
 
-const handleUnauthorized = () => {
-  localStorage.clear();
-  window.location.href = PATH.HOME;
-}
-
 const authorizationClient = axios.create({
   baseURL: API.BASE_URL,
   withCredentials: true, // 요청보낼 때 자격 증명 정보(인증 헤더 등)을 포함시킴 (origin이 달라도)
-});
-
-authorizationClient.interceptors.request.use((config) => {
-  return Object.assign(config, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-    },
-  });
 });
 
 //accesstoken 가져오고 있는지 상태 여부 
@@ -45,20 +32,16 @@ function onAccessTokenFetched(accessToken: string) {
   subscribers = [];
 }
 
-authorizationClient.interceptors.response.use((response)=>{
-  return response;
-}, 
-async (error) => {
-  if(error.response.data.errorCode === 401 && localStorage.getItem("accessToken")) {
-    return "1";
-  }
-  return Promise.reject(error);
-})
+function handleUnauthorized () {
+  localStorage.clear();
+  window.location.href = PATH.HOME;
+}
 
 async function resetTokenAndReattemptRequest(error: any) {
   try {
     const {response: errorResponse} = error;
 
+    //엑세스토큰 얻었을 때 원래 요청 재시도 
     const retryOriginalRequest = new Promise((resolve, reject) => {
       addSubscribers(async (accessToken)=>{
         try {
@@ -70,6 +53,7 @@ async function resetTokenAndReattemptRequest(error: any) {
       })
     });
 
+    //엑세스토큰 안가져오고 있다면 재발급(REISSUE) 요청 후 받은 토큰 로컬스토리지 저장
     if(!isAlreadyFetchingAccessToken) {
       isAlreadyFetchingAccessToken = true;
       await axios
@@ -89,12 +73,13 @@ async function resetTokenAndReattemptRequest(error: any) {
             onAccessTokenFetched(data.accessToken)
           }
         )
-        .catch((err)=>{
+        .catch((err)=>{ //못받아온다면 로그인 정보 없는걸로 간주
           console.log("로그인 정보 없어 메인 화면으로 이동");
           handleUnauthorized();
           return Promise.reject(err)
         })
     }
+    //엑세스토큰 얻었을 때 원래 요청 재시도 
     return await retryOriginalRequest;
   } catch (refreshError) {
     console.log("로그인 정보 없어 메인 화면으로 이동");
@@ -103,6 +88,33 @@ async function resetTokenAndReattemptRequest(error: any) {
   }
 }
 
+//req 인터셉터
+authorizationClient.interceptors.request.use((config) => {
+  return Object.assign(config, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+  });
+}, (error) => {
+  console.error("[Axios]", error);
+  return Promise.reject(error)
+} );
+
+//resp 인터셉터 
+authorizationClient.interceptors.response.use(
+  (response) => { //2xx 범위 응답 성공 시
+  return response;
+}, 
+  async (error) => { //그 외 응답 실패 시 
+    if(error.response.data.errorCode === 401 && localStorage.getItem("accessToken")) { //401인데 로컬스토리지 엑세스토큰이 존재할 때
+      return resetTokenAndReattemptRequest(error); //엑세스토큰 재발급하고 재요청 
+    }
+    return Promise.reject(error); //인증 에러가 아닌 다른 에러
+})
+
+
+
+///////////////////////////UnAthorization 
 const unAuthorizationClient = axios.create({
   baseURL: API.BASE_URL,
   withCredentials: true,
