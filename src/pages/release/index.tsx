d1dc8aff7ff935dc964ad2bdb87fad90250ca6ee
jyeon <jeongyeon865@gmail.com>
2023-08-07 01:28:49 +0900

    [REFACTOR] 릴리즈노트 테이블 간격 수정, 데이터 형식 수정

--- src/pages/release/index.tsx
+++ src/pages/release/index.tsx
@@ -4,264 +4,79 @@ import {CategoryTitle, Title1} from "../../components/Text/Title";
 import {Table1, HeaderCell, TableRow, TableCell} from "../../components/Table";
 import {Container1} from "../../components/Container";
 
+type Release = {
+    id: number
+    tag: string,
+    version: string,
+    content: string,
+    lastModifierName: string,
+    lastModifierEmail: string,
+    releaseDate: string,
+    lastModifiedTime: string
+}
+
+type Category = {
+    id: number,
+    detail: string,
+    title: string,
+    description: string,
+    lastModifierName: string,
+    lastModifiedTime: string
+}
+
+type CategoryItem = {
+    category: Category
+    release: Release[]
+}
 
 export default function Release() {
     const [project, setProject] = useState("첫회사 첫 프로젝트");
-    const [categories, setCategories] = useState([
+    const [categories, setCategories] = useState<CategoryItem[]>([
         {
             category: {
-                title: "첫 프로젝트 첫 카테고리",
-                description: "아ㅣㄹ넝ㄹㄴ",
-                detail: "null",
-                lastModifierName: "null",
-                lastModifiedTime: "null"
+                "id": 4,
+                "detail": "null",
+                "title": "2번째 프로젝트 1번째 카테고리",
+                "description": "아ㅣㄹ넝ㄹㄴ",
+                "lastModifierName": "null",
+                "lastModifiedTime": "null"
             },
-            release: [
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:10:10.620659",
-                    lastModifierName: "null",
-                    content: "sdflsdsdflksjdflskdjfsldkfjsklfjsdklfjsflksjdflskdjfsldkfjsklfjsdklfjsksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjssdflksjdflskdjfsldkfjsklfjsdklfjsksdflksjdflskdjfsldkfjsklfjsdklfjsldfjsdlkf" +
-                        "sldfsdflksjdflskdjfsldkfjsklfjsdklfjssdflksjdflskdjfsldkfjsklfjsdklfjskjsldfkj" +
-                        "sdlsdflksjdflskdjfsldkfjsklfjsdklfjssdflksjdflskdjfsldkfjsklfjsdklfjskfjs",
-                    version: "1.1.0",
-                    tag: "DEPRECATED"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:10:25.007838",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "NEW"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:10:29.561621",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "UPDATED"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:10:34.006635",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "FIXED"
-                }
-            ]
+            release: []
         },
         {
             category: {
-                title: "첫번째 프로젝트 두번째 카테고리",
-                description: "아ㅣㄹ넝ㄹㄴ",
-                detail: "null",
-                lastModifierName: "null",
-                lastModifiedTime: "null"
+                "id": 5,
+                "detail": "null",
+                "title": "2번째 프로젝트 2번째 카테고리",
+                "description": "아ㅣㄹ넝ㄹㄴ",
+                "lastModifierName": "null",
+                "lastModifiedTime": "null"
             },
-            release: [
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:10:50.751726",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "NEW"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:10:52.610176",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "UPDATED"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:10:54.723116",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "DEPRECATED"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:10:56.309203",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "FIXED"
-                }
-            ]
+            release: []
         },
         {
             category: {
-                title: "첫번째 프로젝트 세번째 카테고리",
-                description: "아ㅣㄹ넝ㄹㄴ",
-                detail: "null",
-                lastModifierName: "null",
-                lastModifiedTime: "null"
+                "id": 6,
+                "detail": "null",
+                "title": "2번째 프로젝트 3번째 카테고리",
+                "description": "아ㅣㄹ넝ㄹㄴ",
+                "lastModifierName": "null",
+                "lastModifiedTime": "null"
             },
-            release: [
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:07.544848",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "NEW"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:08.970794",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "FIXED"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:10.549661",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "NEW"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:11.770884",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "FIXED"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:13.117891",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "UPDATED"
-                }
-            ]
-        },
-        {
-            category: {
-                title: "첫번째 프로젝트 네번째 카테고리",
-                description: "아ㅣㄹ넝ㄹㄴ",
-                detail: "null",
-                lastModifierName: "null",
-                lastModifiedTime: "null"
-            },
-            release: [
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:27.182969",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "NEW"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:28.609412",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "DEPRECATED"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:29.905167",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "DEPRECATED"
-                }
-            ]
-        },
-        {
-            category: {
-                title: "첫번째 프로젝트 다섯번째 카테고리",
-                description: "아ㅣㄹ넝ㄹㄴ",
-                detail: "null",
-                lastModifierName: "null",
-                lastModifiedTime: "null"
-            },
-            release: [
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:36.651553",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfsldkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "NEW"
-                },
-                {
-                    releaseDate: "2023-08-01",
-                    lastModifiedTime: "2023-08-01T14:11:38.176223",
-                    lastModifierName: "null",
-                    content: "sdflksjdflskdjfslsdlfksdjfwlsdlkfsjfwelrkjesdkfjsklfjsdklfjs" +
-                        "sdklfjskldfjsdlkf" +
-                        "sldfkjsldfkj" +
-                        "sdlkfjs",
-                    version: "1.1.0",
-                    tag: "FIXED"
-                }
-            ]
+            release: []
         }
     ]);
+
+    type Release = {
+        id: number
+        tag: string,
+        version: string,
+        content: string,
+        lastModifierName: string,
+        lastModifierEmail: string,
+        releaseDate: string,
+        lastModifiedTime: string
+    }
 
     type TagColors = {
         [key: string]: string;
@@ -279,7 +94,7 @@ export default function Release() {
         <Container>
             <MainContainer>
                 <ReleaseCreateTitle>{project}</ReleaseCreateTitle>
-                {categories.map((category, index) =>
+                {categories.map((category: CategoryItem, index: number) =>
                     <ReleaseContainer>
                         <CategoryTitle1 key={index}>{category.category.title}</CategoryTitle1>
                         <ReleaseTable>
@@ -292,7 +107,7 @@ export default function Release() {
                             </tr>
                             </thead>
                             <tbody>
-                            {category.release.map((release, rindex) =>
+                            {category.release.map((release: Release, rindex: number) =>
                                 <ReleaseRow key={rindex}>
                                     <TableCell1>{release.version}</TableCell1>
                                     <TableCell1>{release.releaseDate}</TableCell1>
@@ -357,6 +172,9 @@ export const ReleaseRow = styled(TableRow)`
 
 export const TableCell1 = styled(TableCell)`
   padding: 10px;
+  width: 10rem;
+  min-width: 10rem;
+  max-width: 15rem;
 `
 
 export const TableCellTag = styled.td <{optionTagColor: string}>`
@@ -376,7 +194,8 @@ export const TableCellTag = styled.td <{optionTagColor: string}>`
 `
 
 export const TableCellLong = styled(TableCell)`
-  width: 65%;
+  width: 63rem;
+  min-width: 50rem;
   padding: 10px 50px;
   white-space: normal;
   word-break: break-all;
