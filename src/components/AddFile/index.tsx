bd29d3e44739a7a3d7f42c1584d83ddb1a873618
JoSooha <dlrhdcjs@naver.com>
2023-08-07 21:34:43 +0900

    feat: 파일 업로드 기능 구현 Addfile

--- src/components/AddFile/index.tsx
+++ src/components/AddFile/index.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import COLORS from "../../constants/color";
-import React from "react";
+import React, { useState } from "react";
 
 const LogoContainer = styled.div`
   display: flex;
@@ -8,35 +8,52 @@ const LogoContainer = styled.div`
   min-width: 35rem;
 `;
 
-const AddButton = styled.div`
+const AddButton = styled.input`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 7.3rem;
-  height: 2.6rem;
   color: #000;
   font-family: Inter;
   font-size: 1.2rem;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
-  border: 0.1rem solid ${COLORS.GREY[600]};
   margin-bottom: 0.88rem;
-  cursor: pointer;
+  // cursor: pointer;
+  // border: 0.1rem solid ${COLORS.GREY[600]};
+  // width: 7.3rem;
+  // height: 2.6rem;
 `;
-const AddContainer = styled.div`
-  min-width: 35rem;
-  width: 35rem;
-  height: 17.625rem;
+
+const AddContainer = styled.img`
+  // min-width: 35rem;
+  width: 24rem;
+  height: 24rem;
   border-radius: 0.625rem;
-  border: 0.1rem solid ${COLORS.GREY[300]};
+  border: 0.1rem solid ${COLORS.GREY[100]};
 `;
 
 export default function AddFile() {
+  // TODO: 회사 관리에서 이전 이미지 가져오는 로직 추가해야 
+  const [imageSrc, setImageSrc]: any = useState(null); 
+
+    const onUpload = (e: any) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        return new Promise<void>((resolve) => { 
+            reader.onload = () => {	
+                setImageSrc(reader.result || null); // 파일의 컨텐츠
+                resolve();
+            };
+        });
+    }
+
   return (
     <LogoContainer>
-      <AddButton>첨부하기</AddButton>
-      <AddContainer></AddContainer>
+      <AddButton type="file" accept="image/*" onChange={onUpload}/>
+      <AddContainer src={imageSrc}></AddContainer>
     </LogoContainer>
   );
 }
