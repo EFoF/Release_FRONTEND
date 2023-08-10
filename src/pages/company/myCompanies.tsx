bef8a47c8068d241e0a1ab386d84eb58ca9a6890
JoSooha <dlrhdcjs@naver.com>
2023-08-11 04:14:21 +0900

    feat: mycompany가 없으면 NoCompany 렌더링

--- src/pages/company/myCompanies.tsx
+++ src/pages/company/myCompanies.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import React, {useState, useEffect} from "react"
-import {Title1, Title2} from "../../components/Text/Title";
+import React, { useState, useEffect } from "react";
+import { Title1, Title2 } from "../../components/Text/Title";
 import kakaoenter from "../../img/kakao-enter.jpg";
 import naver from "../../img/naver.png";
 import kakaopay from "../../img/kakao-pay.jpg";
@@ -9,63 +9,77 @@ import eagle from "../../img/icon-park-outline_eagle.png";
 import kakaobank from "../../img/kakao-brain.png";
 import kakaogames from "../../img/kakao-games.jpg";
 import facebook from "../../img/facebook.png";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import PATH from "../../constants/path";
-import {CompanyCard, CompanyContainer, CompanyImage, CompanyListContainer, CompanyName} from "../home";
+import {
+  CompanyCard,
+  CompanyContainer,
+  CompanyImage,
+  CompanyListContainer,
+  CompanyName,
+} from "../home";
 import { getMyCompanies } from "../../api/company";
+import NoCompany from "./NoCompany";
 
 interface Company {
-    id: number;
-    name: string;
-    imageUrl: string;
+  id: number;
+  name: string;
+  imageUrl: string;
 }
 
 export default function MyCompanies() {
-    const [companies, setCompanies] = useState<Company[] | null>(null)
-    useEffect(() => {
-        const fetchMyCompanies = async () => {
-            try {
-                const { content } = await getMyCompanies();
-                console.log("content", content);
-                setCompanies(content);
-              } catch (error) {
-                console.error('Error fetching companies:', error);
-              }
-        };
-        fetchMyCompanies();
-      }, []);
-
-    const navigate = useNavigate();
-
-    const handleCompanyClick = () => {
-        navigate(PATH.COMPANYMAIN);
+  const [companies, setCompanies] = useState<Company[] | null>(null);
+  useEffect(() => {
+    const fetchMyCompanies = async () => {
+      try {
+        const { content } = await getMyCompanies();
+        console.log("content", content);
+        setCompanies(content);
+      } catch (error) {
+        console.error("Error fetching companies:", error);
+      }
     };
+    fetchMyCompanies();
+  }, []);
 
-    const projectClick = () => {
-        navigate(PATH.MYPROJECT);
-    };
+  const navigate = useNavigate();
 
-    return (
-        <Container>
-            <MainContainer>
-                <div style={{display: "flex"}}>
-                    <CompanyTitle>내가 소속된 회사</CompanyTitle>
-                    <ProjectTitle onClick={projectClick}>내가 소속된 프로젝트</ProjectTitle>
-                </div>
-                <CompanyContainer>
-                    <CompanyListContainer>
-                        {companies !== null && companies
-                            .map((company, index) => (
-                                <CompanyCard key={index} onClick={handleCompanyClick}>
-                                    <CompanyImage src={company.imageUrl} alt={company.name}/>
-                                    <CompanyName>{company.name}</CompanyName>
-                                </CompanyCard>
-                            ))}
-                    </CompanyListContainer>
-                </CompanyContainer>
-            </MainContainer>
-        </Container>
-    );
+  const handleCompanyClick = () => {
+    navigate(PATH.COMPANYMAIN);
+  };
+
+  const projectClick = () => {
+    navigate(PATH.MYPROJECT);
+  };
+
+  return (
+    <Container>
+      {companies !== null &&
+        (companies.length === 0 ? (
+          <NoCompany />
+        ) : (
+          <MainContainer>
+            <div style={{ display: "flex" }}>
+              <CompanyTitle>내가 소속된 회사</CompanyTitle>
+              <ProjectTitle onClick={projectClick}>
+                내가 소속된 프로젝트
+              </ProjectTitle>
+            </div>
+            <CompanyContainer>
+              <CompanyListContainer>
+                {companies !== null &&
+                  companies.map((company, index) => (
+                    <CompanyCard key={index} onClick={handleCompanyClick}>
+                      <CompanyImage src={company.imageUrl} alt={company.name} />
+                      <CompanyName>{company.name}</CompanyName>
+                    </CompanyCard>
+                  ))}
+              </CompanyListContainer>
+            </CompanyContainer>
+          </MainContainer>
+        ))}
+    </Container>
+  );
 }
 
 export const Container = styled.div`
