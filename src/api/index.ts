5f286781326d7a1a34a2c76511bdfae3683b55b7
jay <wlguswhd0809@naver.com>
2023-08-12 20:41:26 +0900

    [REFACTOR] cicd test

--- src/api/index.ts
+++ src/api/index.ts
@@ -124,7 +124,8 @@ const unAuthorizationClient = axios.create({
 unAuthorizationClient.interceptors.response.use(
   (response) => {
     //특정 도메인에 대한 접근 허용, 로그인을 하지 않은 인증되지 않은 사용자니까
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000";
+    // response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000";
+    response.headers["Access-Control-Allow-Origin"] = API.BASE_URL;
     return response;
   },
   (error) => {
